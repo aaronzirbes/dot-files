3cdeb671bcf61ad933cb3e23222b11230703f567
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2014-11-11 11:58:54 -0600

    adding sample usage

--- crucible.py
+++ crucible.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
+# Sample:
+# crucible.py CR --title "your title" @reviewer1 @reviewer2
+
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
